887c343b38070cd6e2a1152acff1129c8d217708
Anubhav Mishra <am678854321@gmail.com>
2025-07-29 22:25:07 +0530

    fixed issue with api and setup

--- server/app.ts
+++ server/app.ts
@@ -7,9 +7,19 @@ const initApp = () => {
   app.use(cors());
   app.use(express.json());
 
+
   app.get("/", (req, res) => {
-  res.send("Home route working!");
-});
+    res.send("Home route working!");
+  });
+
+  // Test POST route
+  app.post("/test", (req, res) => {
+    res.json({ message: "POST received", body: req.body });
+  });
+
+  // Mount user routes (empty for now)
+  const userRoutes = require("./routes/userRoutes").default || require("./routes/userRoutes");
+  app.use("/api/users", userRoutes);
 
   return app;
 };
